71c13fe77be0ed2ca2c88d4f70e058b30aec83f9
kyleseven <kyle.senerpida@icloud.com>
2022-11-24 21:30:23 -0600

    Use async PRAW streams to check for new posts

--- notifier-bot/bot.py
+++ notifier-bot/bot.py
@@ -1,34 +1,26 @@
-import datetime
-
 import discord
-from discord.ext import tasks
+from post_streamer import Post, PostStreamer
 
 import config
-from post_fetcher import PostFetcher
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
-post_fetcher: PostFetcher
+post_fetcher: PostStreamer
 
 
 @client.event
 async def on_ready():
     # Set up the post fetcher
     global post_fetcher
-    post_fetcher = PostFetcher(int(round(datetime.datetime.now().timestamp())))
+    post_fetcher = PostStreamer()
+    await post_fetcher.stream_new(notify)
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Reddit Threads"))
-    # Start looping task
-    notify.start()
 
 
-@tasks.loop(seconds=config.search_interval)
-async def notify():
-    global post_fetcher
-    new_posts = await post_fetcher.fetch_new()
+async def notify(post: Post):
     channel = client.get_channel(config.channel_id)
-    for post in new_posts:
-        await channel.send(f"__**{post.title}**__\n<{post.link}>")  # type: ignore
+    await channel.send(f"__**{post.title}**__\n<{post.link}>")  # type: ignore
 
 
 client.run(config.token)
---
+++ notifier-bot/post_streamer.py
@@ -0,0 +1,45 @@
+import asyncio
+from collections import OrderedDict
+
+import asyncpraw
+
+import config
+
+
+class PostStreamer:
+    def __init__(self):
+        self.reddit = asyncpraw.Reddit(
+            client_id=config.reddit_client_id,
+            client_secret=config.reddit_client_secret,
+            user_agent=config.reddit_user_agent
+        )
+
+    async def stream_new(self, callback):
+        await asyncio.gather(*[self.run_watcher_stream(watcher, callback) for watcher in config.watchers])
+
+    async def run_watcher_stream(self, watcher, callback):
+        print(f"[!] Watcher \"{watcher['name']}\" started for r/{watcher['subreddit']}")
+        subreddit = await self.reddit.subreddit(watcher["subreddit"])
+        async for submission in subreddit.stream.submissions(skip_existing=True):
+            if self.watcher_match(watcher, submission):
+                asyncio.create_task(callback(Post(submission)))
+
+    def watcher_match(self, watcher, submission):
+        match watcher["match_mode"]:
+            case "OR":
+                if any(term.casefold() in submission.title.casefold() for term in watcher["search_terms"]):
+                    return True
+            case "AND":
+                if all(term.casefold() in submission.title.casefold() for term in watcher["search_terms"]):
+                    return True
+            case "ALL":
+                return True
+
+        return False
+
+
+class Post:
+    def __init__(self, submission):
+        self.title = submission.title
+        self.link = "https://reddit.com" + submission.permalink
+        self.time = submission.created_utc
